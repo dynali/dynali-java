5009fcc82e6d664649c8981bc039cbd891e70e18
Robert Osuch <robosuch@gmail.com>
2019-08-20 13:49:38 +0200

    Code refactoring and callASync() method added

--- src/com/dynali/DynaliClient.java
+++ src/com/dynali/DynaliClient.java
@@ -9,37 +9,51 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+import static java.lang.Thread.sleep;
 
 public class DynaliClient {
     private static final String ENDPOINT_LIVE = "https://api.dynali.net/nice/";
     private static final String ENDPOINT_DEBUG = "https://debug.dynali.net/nice/";
-    private static final String POSTDATA_MYIP = "{\"action\":\"myip\"}";
 
-    public static String fetchMyIP() throws IOException, DynaliException {
-        var postdata = new StringEntity(POSTDATA_MYIP);
-        String serializedResponse = fetchResponse(postdata, false);
-        var mapper = new ObjectMapper();
+    public static String retrieveMyIP() throws IOException, DynaliException {
+        StringEntity postdata = new StringEntity("{\"action\":\"myip\"}");
+        String serializedResponse = call(postdata, false);
+        ObjectMapper mapper = new ObjectMapper();
         MyIPResponse deserializedResponse = mapper.readValue(serializedResponse, MyIPResponse.class);
-        if (deserializedResponse.getCode() == 200)
+        if (deserializedResponse.getCode() == 200) {
             return deserializedResponse.getData().getIp();
-        else throw new DynaliException(deserializedResponse.getCode(), deserializedResponse.getMessage());
+        }
+        throw new DynaliException(deserializedResponse.getCode(), deserializedResponse.getMessage());
     }
 
-    private static String fetchResponse(StringEntity params, boolean devEnv) throws IOException {
-        HttpPost request = prepareRequest(params, devEnv);
+    private static String call(StringEntity params, boolean useDevEnv) throws IOException {
+        HttpPost request = prepareRequest(params, useDevEnv);
         HttpResponse response = handleRequest(request);
         return new BasicResponseHandler().handleResponse(response);
     }
 
-    private static HttpPost prepareRequest(StringEntity params, boolean devEnv) {
-        HttpPost request = chooseURL(devEnv);
-        request.addHeader("content-type", "application/x-www-form-urlencoded");
+    private static CompletableFuture<String> callASync(StringEntity params, boolean useDevEnv) {
+        return CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return call(params, useDevEnv);
+                    } catch (IOException e) {
+                        return "failed";
+                    }
+                }
+        );
+    }
+
+    private static HttpPost prepareRequest(StringEntity params, boolean useDevEnv) {
+        HttpPost request = chooseURL(useDevEnv);
+        request.addHeader("content-type", "application/json");
         request.setEntity(params);
         return request;
     }
 
-    private static HttpPost chooseURL(boolean devEnv) {
-        return new HttpPost(devEnv ? ENDPOINT_DEBUG : ENDPOINT_LIVE);
+    private static HttpPost chooseURL(boolean useDevEnv) {
+        return new HttpPost(useDevEnv ? ENDPOINT_DEBUG : ENDPOINT_LIVE);
     }
 
     private static HttpResponse handleRequest(HttpPost request) throws IOException {
@@ -47,9 +61,17 @@ public class DynaliClient {
         return httpClient.execute(request);
     }
 
-    public static void main(String[] args) throws IOException, DynaliException {
-        System.out.println(fetchMyIP());
+    public static void main(String[] args) throws IOException, DynaliException, InterruptedException {
+//        System.out.println(retrieveMyIP());
+        callASync(new StringEntity("{\"action\":\"myip\"}"), false).thenAcceptAsync((resultValue) -> {
+                    System.out.println(resultValue);
+                }
+        );
+        System.out.println("This message should be before the response");
+        while (true) {
+            System.out.println("running in fg...");
+            sleep(1000);
+        }
     }
-
 
 }
